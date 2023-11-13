7f0abf7c66b10d0cc06a68b583867db36ccc4dfa
sabiss <sabrinabezsilva@gmail.com>
2023-11-13 18:10:12 -0300

    instalando cors

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json(), cors());
 app.listen(3000, () => {
   console.log("A API está rodando na porta 3000");
 });
