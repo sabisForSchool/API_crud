32c53def6e4adc1d2dd3c07fd92bdfc2dd61fb03
sabiss <sabrinabezsilva@gmail.com>
2023-11-13 19:59:21 -0300

    rota de login e listagem de usuários

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,16 +1,28 @@
 import usuarioModel from "../models/usuarioModel.js";
 
-class usuarioController {
+class UsuarioController {
   async login(req, res) {
     const { email, senha } = req.body;
-
     try {
       const usuario = await usuarioModel.logar(email, senha);
-      return res.status(200).send(usuario);
+      if (usuario.length == 0) {
+        return res.status(404).send({ message: "Email ou senha incorretos" });
+      }
+      return res.status(200).send(usuario[0]);
     } catch (error) {
-      return res.status(404).send({ message: "Erro ao logar" });
+      return res.status(404).send({ message: `Erro ao logar - ${error}` });
+    }
+  }
+  async listar(req, res) {
+    try {
+      const usuarios = await usuarioModel.listar();
+      return res.status(200).send(usuarios);
+    } catch (error) {
+      return res
+        .status(500)
+        .send({ message: `Erro ao listar usuários - ${error}` });
     }
   }
 }
 
-export default usuarioController;
+export default new UsuarioController();
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,8 +3,15 @@ import conexaoBancoDeDados from "../database/db_connection.js";
 class usuarioModel {
   async logar(email, senha) {
     const conexao = await conexaoBancoDeDados.conectar();
-    const comandoSql = "SELECT * FROM Usuarios WHERE email = ? AND senha = ?";
-    return await conexao.query(comandoSql, [email, senha]);
+    const comandoSql = `SELECT * FROM usuarios WHERE email = '${email}' AND senha = '${senha}'`;
+    const res = await conexao.query(comandoSql);
+    return res.rows;
+  }
+  async listar() {
+    const conexao = await conexaoBancoDeDados.conectar();
+    const comandoSql = "SELECT * FROM usuarios";
+    const usuarios = await conexao.query(comandoSql);
+    return usuarios.rows;
   }
 }
-export default usuarioModel;
+export default new usuarioModel();
