4025d28ccba3190cf0d3ee28fc3c1bc5f35d0e9a
sabiss <sabrinabezsilva@gmail.com>
2023-11-14 14:59:27 -0300

    nova rota para login

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
 import usuario from "./src/routes/usuarioRoutes.js";
+import login from "./src/routes/login.js";
 
 const app = express();
 
-app.use(cors("*"), express.json(), usuario);
+app.use(cors("*"), express.json(), usuario, login);
 app.listen(3000, () => {
   console.log("A API está rodando na porta 3000");
 });
