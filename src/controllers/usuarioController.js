d2a5cac123a428d2485d44fd4de5eab5be6b0a98
sabiss <sabrinabezsilva@gmail.com>
2023-11-15 14:10:25 -0300

    método de listar os pedidos de um usuário específico

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -12,6 +12,7 @@ class UsuarioController {
     }
   }
   async cadastrar(req, res) {
+    console.log("oi");
     const { email, senha } = req.body;
     if (!email) {
       return res.status(400).send({ message: "Digite seu email" });
@@ -20,12 +21,38 @@ class UsuarioController {
       return res.status(400).send({ message: "Digite sua senha" });
     }
     try {
-      await usuarioModel.cadastrar(email, senha);
+      const resultado = await usuarioModel.cadastrar(email, senha);
       return res
         .status(201)
         .send({ message: "Usuário cadastrado com sucesso" });
     } catch (error) {
       res.status(400).send({ message: `Erro ao cadastrar Usuário - ${error}` });
+    }
+  }
+  async listarPedidos(req, res) {
+    const idUsuario = req.params.id;
+    try {
+      const pedidos = await usuarioModel.listarPedidos(idUsuario);
+      if (pedidos.length === 0) {
+        return res.status(404).send({ pedidos: false, message: "Sem pedidos" });
+      }
+
+      let precoTotal = 0;
+      let nomesProdutos = [];
+
+      for (let pedido of pedidos) {
+        const produto = await usuarioModel.buscaProduto(pedido.id_produto);
+        precoTotal += produto[0].preco;
+        nomesProdutos.push(produto[0].nome);
+      }
+
+      return res
+        .status(200)
+        .send({ preco: precoTotal, produtos: nomesProdutos });
+    } catch (error) {
+      return res
+        .status(500)
+        .send({ message: `Erro ao listar pedidos - ${error}` });
     }
   }
 }
