0c895cdebb6e23ecbe4307c3312a95c220ba1c03
sabiss <sabrinabezsilva@gmail.com>
2023-11-15 14:11:27 -0300

    rota de cadastro e pedidos

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -3,6 +3,9 @@ import usuarioController from "../controllers/usuarioController.js";
 
 const routes = new Router();
 
-routes.get("/", usuarioController.listar);
+routes
+  .get("/", usuarioController.listar)
+  .get("/pedidos/:id", usuarioController.listarPedidos)
+  .post("/cadastro", usuarioController.cadastrar);
 
 export default routes;
